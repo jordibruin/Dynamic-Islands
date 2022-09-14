2e529ba5ba8e1b04a348da72f5242198c17fc558
Jordi Bruin <jordi@goodsnooze.com>
2022-09-15 00:07:21 +0200

    initial commit

---
+++ Dynamic Islands/Dynamic Islands/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Dynamic Islands
+//
+//  Created by Jordi Bruin on 15/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dynamic Islands/Dynamic Islands/Dynamic_IslandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_IslandsApp.swift
+//  Dynamic Islands
+//
+//  Created by Jordi Bruin on 15/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_IslandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
